1b05413b31ed341ad226df2451d6103b14c3af73
vinodini1407 <vinodinikoduru@gmail.com>
2024-10-21 11:55:51 +0530

    Create voteeligible.c

---
+++ voteeligible.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("Enter the value of a");
+    scanf("%d",&a);
+    if(a>=18)
+    {
+        printf("Person is eligible to vote ");
+    }
+    }
