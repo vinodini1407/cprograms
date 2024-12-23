f607d09f4de925482653241bd0388037e8d55206
vinodini1407 <vinodinikoduru@gmail.com>
2024-12-23 12:10:26 +0530

    Create toprintsumofnumbers1tonusingloop.c

---
+++ toprintsumofnumbers1tonusingloop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main()
+{
+    int i,n,sum=0;
+    printf("enter the value of n");
+    scanf("%d",&n);
+    for(i=1;i<=n;++i)
+    {
+        sum=sum+i;
+    }
+    printf("the sum is %d",sum);
+}
