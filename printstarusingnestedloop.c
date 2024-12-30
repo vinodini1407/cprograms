89c0baac445a9ebdd290db9ebe05e832bc06170a
vinodini1407 <vinodinikoduru@gmail.com>
2024-12-30 11:26:26 +0530

    Update printstarusingnestedloop.c

--- printstarusingnestedloop.c
+++ printstarusingnestedloop.c
@@ -2,9 +2,9 @@
 void main()
 {
     int i,j;
-    for(i=0;i<=5;++i)
+    for(i=1;i<=5;++i)
     {
-         for(j=0;j<=5;++j)
+         for(j=1;j<=5;++j)
          {
              printf("*");
          }
